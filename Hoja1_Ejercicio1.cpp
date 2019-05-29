9f7328846cbe3b28a928f2ffae6829ae79b08227
Augusto Morante <48858576+AugustoMorante@users.noreply.github.com>
2019-05-29 17:37:28 -0500

    Add files via upload

---
+++ Hoja1_Ejercicio1.cpp
@@ -0,0 +1,40 @@
+
+#include "pch.h"
+#include <iostream>
+#include <conio.h>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+	int *notas;
+	int numeroMayor = 21, numeroMenor = 0, k = 0;
+	notas = new int[6];
+	for (int i = 0; i < 6; i++)
+	{
+		do
+		{
+			cout << "Ingrese nota " << i + 1 << ": ";
+			cin >> notas[i];
+		} while (notas[i] < 0 || notas[i] > 20);
+		if (notas[i] == numeroMenor)
+		{
+			k++;
+		}
+		if (notas[i] < numeroMayor)
+		{
+			numeroMenor = notas[i];
+		}
+		numeroMayor = notas[i];
+	}
+	cout << endl;
+	cout << "La nota que no se considera es: " << numeroMenor << endl;
+	if (k > 0)
+	{
+		cout << "Solo se elimina un solo " << numeroMenor;
+	}
+	delete notas;
+	_getch();
+}
+
+
